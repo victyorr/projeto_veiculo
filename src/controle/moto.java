d0d11c865061468f974fb3b5be5b9e36444f4aad
Victor Augusto Vieira <126778778+victyorr@users.noreply.github.com>
2023-03-09 18:26:37 -0300

    Add files via upload

---
+++ src/controle/moto.java
@@ -0,0 +1,16 @@
+package controle;
+
+public class moto extends veiculo {
+
+	public moto(int motor, int freio, boolean ativacao, int cambio) {
+		super(motor, freio, ativacao, cambio);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void acelerar(int tempo) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
